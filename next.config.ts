d455037f66f2eae3c282594ecb25412af4db3e49
sidhyaashu <ashutoshsidhya69@gmail.com>
2025-09-21 02:47:03 +0530

    x

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-// File: next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
